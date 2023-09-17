a5283264d6688203cb2670b0375ba0b0b8e19c3d
sobuhasy <sobu.hasy@hotmail.com>
2023-09-17 13:13:27 +0200

    first code

---
+++ Quelle1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class Fursuiter {
+	string fursonaName;
+	string Species;
+	string fursuitMaker;
+	int fursuiterAge;
+};
+
+int main() {
+	Fursuiter fursuiter1;
+	fursuiter1.
+}
