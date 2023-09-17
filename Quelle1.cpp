b97e344a27e26d5877a8f8655bb6dc2bf4097b98
sobuhasy <sobu.hasy@hotmail.com>
2023-09-17 13:28:23 +0200

    created the first try

--- Quelle1.cpp
+++ Quelle1.cpp
@@ -2,13 +2,23 @@
 using namespace std;
 
 class Fursuiter {
+public:
 	string fursonaName;
 	string Species;
 	string fursuitMaker;
 	int fursuiterAge;
+
+	void IntroduceYourself() {
+		cout << "Hello, my name is " << fursonaName << ", I am a " << fursuiterAge << " year old " << Species << ", and my Fursuit was made by " << fursuitMaker << "." << endl;
+	}
 };
 
 int main() {
 	Fursuiter fursuiter1;
-	fursuiter1.
+	fursuiter1.fursonaName = "SobuHasy";
+	fursuiter1.Species = "bunny";
+	fursuiter1.fursuitMaker = "R5Suits";
+	fursuiter1.fursuiterAge = 25;
+	fursuiter1.IntroduceYourself();
+	cout << "It is a pleasure to be in the Furry Fandom and to program things!" << endl;
 }
